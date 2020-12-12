5cec96c5d50bfc5516e041a9557495c0157030b8
Ulil1249 <ulil.absor1249@gmail.com>
2020-12-12 08:43:19 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Mob.App_Webgis" 
---
+++ appConfig.js
@@ -0,0 +1,49 @@
+var developmentDatabase = {
+    postgres: {
+    host: 'localhost',
+    port: 5432,
+    database: 'database_name',
+    user: 'postgres',
+    password: 'postgres'
+    }
+    }
+    
+    var connectionString = "dklcaufskcmxlw:a23b019e2cc6b80169d4238d7fc63a02971e34fba5cda2651471d00b88ca1901@ec2-54-82-208-124.compute-1.amazonaws.com:5432/d11i9247c8ukpr?ssl=true";
+    if (process.env.NODE_ENV == 'production') {
+    //Production mode
+    if (process.env.DATABASE_URL) {
+    developmentDatabase =
+    parseConnectionString(process.env.DATABASE_URL);
+    } else {
+    console.log("process.env.DATABASE_URL empty, connectionStringvariable used");
+    developmentDatabase = parseConnectionString(connectionString);
+    }
+    }else{
+    //Development mode
+    developmentDatabase = parseConnectionString(connectionString);
+    }
+    function parseConnectionString(connectionString) {
+    if (connectionString) {
+    var myRegexp = /(\w+):(\w+)@(.+):(\w+)\/(\w+)/g;
+    var match = myRegexp.exec(connectionString);
+    if (match.length == 6) {
+    developmentDatabase.postgres.user = match[1];
+    developmentDatabase.postgres.password = match[2];
+    developmentDatabase.postgres.host = match[3];
+    developmentDatabase.postgres.port = Number(match[4]);
+    developmentDatabase.postgres.database = match[5];
+    developmentDatabase.postgres.ssl = { rejectUnauthorized: false };
+    return developmentDatabase;
+    }
+    }
+    console.log("connectionString cannot be parsed");
+    return null;
+    }
+    module.exports = {
+    hostname: "http://localhost",
+    port: 5656,
+    database: {
+    postgres: developmentDatabase.postgres
+    }
+    }
+    
